fefe97787c19d12faa6b4223fb58c55d8fab4be2
HansaraHettiarachchi <hansarasasanka@gmail.com>
2025-08-07 10:33:32 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
-import Home from './Home';
+import Home from './pages/Home';
 import Header from './comp/Header';
 import { Container } from 'react-bootstrap';
 import Footer from './comp/Footer';
