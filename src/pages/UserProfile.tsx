5f64880e838c6dd9ea44ce768cff8bce3e4f91b3
HansaraHettiarachchi <hansarasasanka@gmail.com>
2025-08-07 20:52:01 +0530

    test

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const UserProfile = () => {
   return (
-    <div>UserProfileyu</div>
+    <div>UserProfile
+      
+      dsfdsds sfds dsfdsfdsf sfds  dsds dsfdsfs
+      
+      yu</div>
   )
 }
 
