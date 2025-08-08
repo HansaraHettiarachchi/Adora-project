e5ebab4f047551729a197f24ddcf6c5dd620d64c
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-08-08 19:06:19 +0530

    update

--- src/components/Home/TopSellingFlowers.tsx
+++ src/components/Home/TopSellingFlowers.tsx
@@ -1,29 +1,47 @@
 import React from 'react';
+import home1 from '../../assets/images/home-img1.jpeg';
+import home2 from '../../assets/images/home-img2.jpeg';
+import home3 from '../../assets/images/home-img3.jpeg';
+import home4 from '../../assets/images/home-img4.jpg';
+
+const flowerData = [
+  { img: home1, alt: 'Home Flower', name: 'Featured Flower', price: '' },
+  { img: home2, alt: 'Candelabra Aloe', name: 'Candelabra Aloe', price: '$29' },
+  { img: home3, alt: 'Golden Pothos', name: 'Golden Pothos', price: '$39' },
+  { img: home4, alt: 'Homalomena', name: 'Homalomena', price: '$19' },
+];
+
+const imageStyle: React.CSSProperties = {
+  width: '180px',
+  height: '180px',
+  objectFit: 'cover',
+  display: 'block',
+  margin: '0 auto 10px auto',
+};
 
 const TopSellingFlowers: React.FC = () => {
   return (
-    <div className="text-center mb-5">
+    <div className="text-center mb-5 mt-5">
       <h2>Top Selling Flowers</h2>
-      <div className="row">
-        <div className="col-md-3">
-          <img src="/path/to/snake-plant.jpg" alt="Snake Plant" className="img-fluid mb-2" />
-          <p>Snake Plant - $49</p>
-          <button className="btn btn-success btn-sm">Add to Cart</button>
-        </div>
-        <div className="col-md-3">
-          <img src="/path/to/candelabra-aloe.jpg" alt="Candelabra Aloe" className="img-fluid mb-2" />
-          <p>Candelabra Aloe - $29</p>
-          <button className="btn btn-success btn-sm">Add to Cart</button>
-        </div>
-        <div className="col-md-3">
-          <img src="/path/to/golden-pothos.jpg" alt="Golden Pothos" className="img-fluid mb-2" />
-          <p>Golden Pothos - $39</p>
-          <button className="btn btn-success btn-sm">Add to Cart</button>
-        </div>
-        <div className="col-md-3">
-          <img src="/path/to/homalomena.jpg" alt="Homalomena" className="img-fluid mb-2" />
-          <p>Homalomena - $19</p>
-          <button className="btn btn-success btn-sm">Add to Cart</button>
+      <div className="flowers-container py-3 px-2" style={{ background: '#f8f9fa', borderRadius: '12px' }}>
+        <div className="row">
+          {flowerData.map((flower, idx) => (
+            <div className="col-md-3 d-flex flex-column align-items-center" key={idx}>
+              <img
+                src={flower.img}
+                alt={flower.alt}
+                style={imageStyle}
+                className="mb-2"
+              />
+              <p>
+                {flower.name}
+                {flower.price && ` - ${flower.price}`}
+              </p>
+              <div className="w-100 d-flex justify-content-center">
+                <button className="btn btn-success btn-sm">Add to Cart</button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <button className="btn btn-outline-success mt-3">View More</button>
