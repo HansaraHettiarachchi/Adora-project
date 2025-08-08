f6ed147aca611b37d04cbae2fb7705b8ec5f3f03
HansaraHettiarachchi <hansarasasanka@gmail.com>
2025-08-08 19:59:14 +0530

    Sachini First Commit

---
+++ src/components/Checkout/CheckoutDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function CheckoutDetails() {
+    return (
+        <>
+            <Container className='mt-3'>
+                {/* Code here */}
+                    fdsdsdsdsdsdf
+                    fdsfdsfdsf
+                    dsfdsfdsfdsf
+                    fsffdfsdf
+                    dsfdsfdsfdsfdsf
+
+            </Container>
+        </>
+    )
+}
