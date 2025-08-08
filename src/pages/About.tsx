f99c9635d1861de1eb7d69666201a9e3f17f7e2c
Nipuni Weerasuriya <nipuniweerasuriya2@gmail.com>
2025-08-08 17:05:13 +0530

    Add image carousel to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,59 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Container, Row, Col,Carousel } from 'react-bootstrap';
 
 const About = () => {
     return (
         <>
             <Header />
 
-            <Container className='d-flex justify-content-center'>
-                <Container className='bg-black' style={{ width: "100%", height: "100%" }}>
-                // Create Your design here
-
-                    
-                </Container>
-            </Container>
-
+            {/* Top Carousel */}
+            <Carousel fade interval={30000}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 about-carousel-img"
+                        style={{ height: '500px', objectFit: 'cover' }}
+                        src="src/assets/images/about_slide (1).png"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 about-carousel-img"
+                        style={{ height: '500px', objectFit: 'cover' }}
+                        src="src/assets/images/about_slide (2).jpg"
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 about-carousel-img"
+                        style={{ height: '500px', objectFit: 'cover' }}
+                        src="src/assets/images/about_slide (3).jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 about-carousel-img"
+                        style={{ height: '500px', objectFit: 'cover' }}
+                        src="src/assets/images/about_slide (4).jpg"
+                        alt="Fourth slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 about-carousel-img"
+                        style={{ height: '500px', objectFit: 'cover' }}
+                        src="src/assets/images/about_slide (5).jpg"
+                        alt="Fifth slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
             <Footer />
         </>
-    )
-}
+    );
+};
 
-export default About
+export default About;
