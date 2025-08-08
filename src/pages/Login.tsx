d59ffd6412c8546c89ed6a5a59e8e2b88250207b
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-08-08 11:51:23 +0530

    login css change

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,19 +2,12 @@ import React from 'react';
 import LoginForm from '../components/Login/LoginForm';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './css/login.css';
 
 const Login: React.FC = () => (
     <>
         <Header />
-        <div
-            className="d-flex justify-content-center align-items-center vh-100"
-            style={{
-                backgroundImage: "url('/src/assets/images/banner2.png')",
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover', // Added this line
-            }}
-        >
+        <div className="login-background">
             <LoginForm />
         </div>
         <Footer />
