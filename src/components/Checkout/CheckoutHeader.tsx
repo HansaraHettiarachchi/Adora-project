ade135efa8bc36423c212465a01f965929de9819
Nipuni Weerasuriya <nipuniweerasuriya2@gmail.com>
2025-08-11 14:22:05 +0530

    Enhance checkout page layout and styling

--- src/components/Checkout/CheckoutHeader.tsx
+++ src/components/Checkout/CheckoutHeader.tsx
@@ -5,27 +5,19 @@ import { MdOutlineArrowRightAlt } from "react-icons/md";
 export default function CheckoutHeader() {
   return (
     <>
-      <div className="d-flex justify-content-between my-5">
+      <div className="d-flex flex-column flex-md-row justify-content-between align-items-center text-center my-5">
         <p style={{ color: "#23B540" }}>
           <Bs1Circle size={20} color="#23B540" className="me-2" /> Shipping Cart
         </p>
         <p>
-          <MdOutlineArrowRightAlt
-            style={{
-              color: "#23B540",
-            }}
-          />
+          <MdOutlineArrowRightAlt style={{ color: "#23B540" }} />
         </p>
         <p style={{ color: "#23B540" }}>
           <Bs2Circle size={20} color="#23B540" className="me-2" /> Checkout
           Details
         </p>
         <p>
-          <MdOutlineArrowRightAlt
-            style={{
-              color: "#A6A3A3",
-            }}
-          />
+          <MdOutlineArrowRightAlt style={{ color: "#A6A3A3" }} />
         </p>
         <p style={{ color: "#A6A3A3" }}>
           <Bs3Circle size={20} color="#A6A3A3" className="me-2" /> Order
