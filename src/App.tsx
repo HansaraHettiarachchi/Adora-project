4af4c125c9f8ac686e8e2de8b96cbdfa3574a201
Nipuni Weerasuriya <nipuniweerasuriya2@gmail.com>
2025-08-12 16:39:30 +0530

    Add Order Complete page and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import User from './pages/User';
-import { Shop } from './pages/Shop';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import About from './pages/About';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import AdminBody from './components/admin/AdminBody';
-import Dashboard from './pages/Admin/Dashboard';
-import Orders from './pages/Admin/Orders';
-import Products from './pages/Admin/Products';
-import Customers from './pages/Admin/Customers';
-import Comments from './pages/Admin/Comments';
-import ResetPasswords from './pages/Admin/ResetPasswords';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import { Shop } from "./pages/Shop";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import OrderComplete from "./pages/OrderComplete";
+import AdminBody from "./components/admin/AdminBody";
+import Dashboard from "./pages/Admin/Dashboard";
+import Orders from "./pages/Admin/Orders";
+import Products from "./pages/Admin/Products";
+import Customers from "./pages/Admin/Customers";
+import Comments from "./pages/Admin/Comments";
+import ResetPasswords from "./pages/Admin/ResetPasswords";
 
 const App: React.FC = () => {
   return (
@@ -37,8 +36,9 @@ const App: React.FC = () => {
         <Route path="/register" element={<Signup />} />
         <Route path="/checkout-details" element={<Checkout />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/OrderComplete" element={<OrderComplete />} />
 
-         {/* Admin section nested routing */}
+        {/* Admin section nested routing */}
         <Route path="/admin" element={<AdminBody />}>
           {/* <Route index element={<Navigate to="dashboard" replace />} />  */}
           <Route path="dashboard" element={<Dashboard />} />
@@ -47,9 +47,7 @@ const App: React.FC = () => {
           <Route path="customers" element={<Customers />} />
           <Route path="comments" element={<Comments />} />
           <Route path="reset-passwords" element={<ResetPasswords />} />
-
         </Route>
-        
       </Routes>
     </Router>
   );
