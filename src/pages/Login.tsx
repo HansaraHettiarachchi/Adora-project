86aa15912656a130d53134eb0aced737f9e00367
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-08-14 23:07:21 +0530

    admin login fixed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../components/login/LoginForm';
+import LoginForm from '../components/Login/LoginForm';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './css/login.css';
