59b17439dee393806a157c80dd273704db1b69c7
SandaliKodithuwakku <sandali768@gmail.com>
2025-08-15 02:18:30 +0530

    Revert "adding breadcrumb bar to the pages"
    This reverts commit aa8ad0e634129b828dbb4744d7073f70bc1d7cee.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Container, Row, Col, Carousel, Image } from "react-bootstrap";
 import { BsCheck2 } from "react-icons/bs";
-import BreadcrumbBar from '../components/BreadcrumbBar'
-
 
 const initialcarddata = {
   name: " ",
@@ -20,8 +18,6 @@ const About = () => {
   return (
     <>
       <Header />
-
-      <BreadcrumbBar currentPage="About"/>
 
       {/* Top Carousel */}
       <Carousel fade interval={3000}>
