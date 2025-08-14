2109b06475da4f10baada491f3a2652d311cf43d
SandaliKodithuwakku <sandali768@gmail.com>
2025-08-15 01:33:02 +0530

    adding breadcrumb bar to the pages

--- src/components/BreadcrumbBar.tsx
+++ src/components/BreadcrumbBar.tsx
@@ -1,20 +1,35 @@
 import { Breadcrumb, Container } from 'react-bootstrap'
+import { AiFillHome } from 'react-icons/ai';
+import { BsChevronDoubleRight } from 'react-icons/bs';
+import { IoHomeOutline } from "react-icons/io5";
 
-/* test */
-
-export default function BreadcrumbBar() {
+ 
+export default function BreadcrumbBar({ currentPage = "" }) {
   return (
     <>
       <Container fluid className=' py-1' style={{ backgroundColor: "#238f39ff" }}>
-        <Container fluid="sm" className='mt-2'>
-          <Breadcrumb >
-            <Breadcrumb.Item className='text-dark' href="#">Home</Breadcrumb.Item>
-            <Breadcrumb.Item className='text-dark' href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-              Library
-            </Breadcrumb.Item>
-            <Breadcrumb.Item className='text-dark' active>Data</Breadcrumb.Item>
-          </Breadcrumb>
-        </Container>
+        <Container
+        fluid="sm"
+        className="d-flex align-items-center"
+        style={{ gap: "8px" }}
+      >
+        {/* Home Icon */}
+        <IoHomeOutline size={22} color="#000" style={{ cursor: "pointer" }} />
+
+        {/* Separator */}
+        <BsChevronDoubleRight size={18} color="#000" />
+
+        {/* Current Page Text */}
+        <span
+          style={{
+            fontSize: "16px",
+            fontWeight: "500",
+            color: "#000"
+          }}
+        >
+          {currentPage}
+        </span>
+      </Container>
       </Container>
     </>
   )
