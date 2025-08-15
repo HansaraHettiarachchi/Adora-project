a4cf822034a9ca5e71b3744aff4a952968fa2cd5
HansaraHettiarachchi <hansarasasanka@gmail.com>
2025-08-15 22:17:40 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -27,7 +27,7 @@ const App: React.FC = () => {
   return (
     <Router basename="/F5-ecommece-pure-p-1">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/user" element={<User />} />
