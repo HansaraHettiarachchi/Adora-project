0ee9daf92de31dac8e176b43f61f0564db469e7a
SandaliKodithuwakku <sandali768@gmail.com>
2025-08-18 19:58:43 +0530

    adding CORS

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './src/routes/UserRoute.js';
 
 const app = express();
 const PORT = 3000;
 
+// CORS Configuration 
+app.use(cors({
+  origin: 'http://localhost:5174', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Body parser middleware
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello from Express hi it hansra!');
 });
