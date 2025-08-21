d6277d9dc81801d13d1ed5f344d18a7ea23c5284
Himal Nethmina <himal.nethmina2003@gmail.com>
2025-08-21 19:05:28 +0530

    comiiting changes

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import { stockRoutes } from './src/routes/StockRoutes.js';
 import { authenticate } from './src/middleware/auth.js';
 import paymentRoutes from './src/routes/PaymentRoutes.js';
 import supplierRoutes from './src/routes/SuplierRoute.js';
+import cardDetailsRoutes from './src/routes/CardDetailRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -36,7 +37,8 @@ app.use('/api/v1/users', userRouters);
 app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/stock', authenticate, stockRoutes);
 app.use('/api/v1/payment', authenticate, paymentRoutes);
-app.use('/api/v1/supplier',supplierRoutes)
+app.use('/api/v1/supplier',supplierRoutes);
+app.use('/api/v1/card', cardDetailsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
